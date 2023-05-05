8d618c5eafc290d455734c247df77b80904bc564
Shubham Verma <vshubham799@gmail.com>
2023-05-04 22:44:14 -0700

    Changes: title changes

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ export default function App() {
     <>
       {!authenticated && (
         <>
-          <h1>Login</h1>
+          <h1>To Do Create App</h1>
           <button type="button" onClick={() => setAuthenticated(true)}>
             Click here to login
           </button>
